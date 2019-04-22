6e009acac86bdb5b01e487d815fd32f3d732486a
iiuex <iiu@adorsys.com.ua>
2019-04-22 03:50:37 +0200

    Imports fixed

--- rest-task3/src/main/java/com/iuminov/controller/DateTimeController.java
+++ rest-task3/src/main/java/com/iuminov/controller/DateTimeController.java
@@ -1,11 +1,8 @@
 package com.iuminov.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
